b47df2f3e4b5a3e640dbb2de00d37a8d25f6ee2e
Kazuya Ueoka <info@fromkk.info>
2024-06-11 16:51:20 +0900

    Initial Commit

---
+++ File/FileApp.swift
@@ -0,0 +1,17 @@
+//
+//  FileApp.swift
+//  File
+//
+//  Created by Kazuya Ueoka on 2024/06/11.
+//
+
+import SwiftUI
+
+@main
+struct FileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
